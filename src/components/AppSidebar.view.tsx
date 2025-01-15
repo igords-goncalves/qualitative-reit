b9e86a968b7493b506e05f567e65e571d44ebf47
Igor Gonçalves <igordossantos38@gmail.com>
2025-01-14 22:39:24 -0300

    feat: enhance AppSidebarView with dynamic menu items and improved structure

--- src/components/AppSidebar.view.tsx
+++ src/components/AppSidebar.view.tsx
@@ -1,13 +1,58 @@
 import { PropsWithChildren } from "react";
-import { Sidebar, SidebarContent, SidebarHeader } from "./ui/sidebar";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+import { Calendar, Home, Inbox } from "lucide-react";
 
 type AppSidebarViewProps = PropsWithChildren;
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+];
 
 const AppSidebarView = ({}: AppSidebarViewProps) => {
   return (
     <Sidebar>
-      <SidebarHeader about="Strings"/>
-      <SidebarContent />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Features</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
     </Sidebar>
   );
 };
