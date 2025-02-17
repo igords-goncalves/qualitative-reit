5331c78de73272913618048bfe091b1c46af46ca
Igor Gonçalves <igordossantos38@gmail.com>
2025-02-17 10:46:57 -0300

    feat: refactor GET request to create query string from parameters and improve error handling

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -1,17 +1,47 @@
-export async function GET() {
+function createQueryString(req: Request) {
+  const { searchParams } = new URL(req.url);
+
+  const search = searchParams.get("search") || "%7B%7D";
+  const page = searchParams.get("page");
+  const take = searchParams.get("take");
+  const CategoryType = searchParams.get("CategoryType");
+
+  if (!search || !page || !take || !CategoryType)
+    return new Response(JSON.stringify({ error: "Missing parameters" }), {
+      status: 400,
+    });
+  const queryString = new URLSearchParams({
+    search,
+    page,
+    take,
+    CategoryType,
+  }).toString();
+
+  return queryString;
+}
+
+export async function GET(req: Request) {
+  const queryString = createQueryString(req);
   try {
-      const response = await fetch('https://statusinvest.com.br/category/advancedsearchresultpaginated?search=%7B%7D&page=02&take=50&CategoryType=2');
-      const data = await response.json();
-      return new Response(JSON.stringify(data), {
-          status: 200,
-          headers: { 'Content-Type': 'application/json' },
-      });
+    const response = await fetch(
+      `https://statusinvest.com.br/category/advancedsearchresultpaginated?${queryString}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Request error on proxy API.");
+    }
+
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
-      console.error('Erro ao buscar dados:', error);
-      return new Response(JSON.stringify({ error: 'Erro ao buscar dados.' }), {
-          status: 500,
-          headers: { 'Content-Type': 'application/json' },
-      });
+    console.error("Erro ao buscar dados:", error);
+    return new Response(JSON.stringify({ error: "Error fetching data." }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
 
